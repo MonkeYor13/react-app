46c7efa609b5349ed73a1e4509d01cd0062c66c6
“MonkeYor13” <monkeyor13@gmail.com>
2024-07-19 17:29:29 -0300

    learning usestate

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,6 +1,8 @@
 //aqui aprendioemos como recibir los datos de una lista desde le padre como props
 //aprendemos como capturar un dato atraves de un evento
 
+import { useState } from "react";
+
 type Props = {
   data: string[];
 };
@@ -8,17 +10,19 @@ type Props = {
 //recuerda que cada lista o array debe llevar una id (key) unico al usar .map, en esta caso como no vienen de una DB le colocaremos el mismo nombre como key(element)
 
 function List({ data }: Props) {
-  const handleClick = (e: string) => {
-    console.log(e);
+  const [index, setIndex] = useState(1);
+
+  const handleClick = (i: number) => {
+    setIndex(i);
   };
 
   return (
     <ul className="list-group">
-      {data.map((element) => (
+      {data.map((element, i) => (
         <li
-          onClick={() => handleClick(element)}
+          onClick={() => handleClick(i)}
           key={element}
-          className="list-group-item"
+          className={`list-group-item ${index == i ? "active" : ""}`}
         >
           {element}
         </li>
